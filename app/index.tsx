4542da2db8e5503a7941a13b6b541e117839b7b3
puspakirana <puspakiranastv@gmail.com>
2025-06-07 17:37:12 +0700

    example of routing and dynamic routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl font-bold text-accent">Welcome!</Text>
+      <Text className="text-5xl font-bold text-primary">Welcome!</Text>
+      <Link href="/onboarding">OnBoarding</Link>
+      <Link href="/movie/avangers">Avanger Movies</Link>
     </View>
   );
 }
---
+++ app/onboarding.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const OnBoarding = () => {
+    return (
+        <View>
+            <Text>Onboarding</Text>
+        </View>
+    )
+}
+
+export default OnBoarding
+const styles = StyleSheet.create({})
